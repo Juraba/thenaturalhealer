70f0408ea0c1486d73c60fec27d2a07a9343f0d7
MarmotteIntrepide <intrepid.marmot@gmail.com>
2017-12-13 18:15:07 +0100

    nav_header_main modified

--- app/src/main/java/com/marmot/intrepid/naturalhealer/MainActivity.java
+++ app/src/main/java/com/marmot/intrepid/naturalhealer/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_map) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_inventory) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_grimoire) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_questBook) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_shop) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_info) {
 
         }
 
