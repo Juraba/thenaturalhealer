eda5cbd836e1a0eef55c701123621c7823b26720
Sissik19 <mini.sissi@yahoo.fr>
2017-12-19 11:42:35 +0100

    Crétation class du diagramme de class avec rprépation des Ãméthodes

---
+++ app/src/main/java/grimoire/Herb.java
@@ -0,0 +1,36 @@
+package grimoire;
+
+/**
+ * Created by Camille K on 19/12/2017.
+ */
+
+public class Herb {
+    private String race;
+    private String rarity;
+    private String history;
+    private String combination;
+    private String type;
+    //Ajouter discover
+    private boolean discover;
+
+    public Herb(String race, String rarity, String history, String combination, String type){
+        this.race = race;
+        this.rarity = rarity;
+        this.history = history;
+        this.combination = combination;
+        this.type = type;
+        this.discover = false;
+    }
+
+    public void setDiscover(){
+        if(!this.discover){
+            this.discover = true;
+        }
+    }
+
+    public boolean isDiscovered(){
+        return discover;
+    }
+
+
+}
---
+++ app/src/main/java/player/Rank.java
@@ -0,0 +1,15 @@
+package player;
+
+/**
+ * Created by Camille K on 19/12/2017.
+ */
+
+public class Rank {
+    private String name;
+    private int goal;
+
+    public Rank(String name, int goal){
+        this.name = name;
+        this.goal = goal;
+    }
+}
